ab9193f7ed29602cffd7d9974986d3527bc627a4
randall <silentvoice5001@gmail.com>
2025-10-10 01:35:25 -0400

    some progress to get app running, but nothing so far

--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
+#include "SDL3/SDL_error.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include "stdio.h"
 #include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+// #include "SDL3_image/SDL_image.h"
 
-int main(int argc, char* argv[]){
+static SDL_Window* window = NULL;
+static SDL_Renderer* render = NULL;
 
-	return 0;
+SDL_AppResult SDL_APPInit(void **appstate, int argc, char* argv[]){
+	
+	SDL_SetAppMetadata("window test", "1.0", "example.com.sample");
+
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Log("couldn't be initialized: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("????FIRST EXAMPLE!!!", 700, 800, SDL_WINDOW_RESIZABLE, &window, &render)) {
+		SDL_Log("window could not be made: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	
+
+	return SDL_APP_CONTINUE;
+
+}
+
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
+    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
+    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
+    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(render, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(render);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(render);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+
+int main(int argc, char *argv[])
+{
 }
