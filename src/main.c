bbb9f315874ae5b4500828b9f3f66f43ba3bb7b2
randall <silentvoice5001@gmail.com>
2025-10-14 23:42:53 -0400

    added example folder, updated ignore file and note.txt, and have a working main. The original main.c is ine example to reference original attempt

--- src/main.c
+++ src/main.c
@@ -1,56 +1,44 @@
-#include "SDL3/SDL_error.h"
-#include "SDL3/SDL_init.h"
-#include "SDL3/SDL_log.h"
-#include "SDL3/SDL_render.h"
-#include "SDL3/SDL_video.h"
-#include "stdio.h"
 #include "SDL3/SDL.h"
-#include "SDL3/SDL_main.h"
-// #include "SDL3_image/SDL_image.h"
+#include <stdio.h>
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* render = NULL;
-
-SDL_AppResult SDL_APPInit(void **appstate, int argc, char* argv[]){
-	
-	SDL_SetAppMetadata("window test", "1.0", "example.com.sample");
-
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_Log("couldn't be initialized: %s", SDL_GetError());
-		return SDL_APP_FAILURE;
-	}
-
-	if (!SDL_CreateWindowAndRenderer("????FIRST EXAMPLE!!!", 700, 800, SDL_WINDOW_RESIZABLE, &window, &render)) {
-		SDL_Log("window could not be made: %s", SDL_GetError());
-		return SDL_APP_FAILURE;
-	}
-
-	
-
-	return SDL_APP_CONTINUE;
-
-}
-
-
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(render, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
-
-    /* clear the window to the draw color. */
-    SDL_RenderClear(render);
-
-    /* put the newly-cleared rendering on the screen. */
-    SDL_RenderPresent(render);
-
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
-}
-
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* render = NULL;
+    
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    if (!SDL_CreateWindowAndRenderer("First Example", 700, 800, SDL_WINDOW_RESIZABLE, &window, &render) != 0) {
+        printf("Window could not be created: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    int running = 1;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = 0;
+            }
+        }
+        
+        const double now = ((double)SDL_GetTicks()) / 1000.0;
+        const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+        const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+        const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+        
+        SDL_SetRenderDrawColorFloat(render, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+        SDL_RenderClear(render);
+        SDL_RenderPresent(render);
+        
+        SDL_Delay(16);
+    }
+    
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
