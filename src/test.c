3ba14780e1788c57fa02978ecd2677a1467f1eee
art <silentvoice5001@gmail.com>
2025-11-01 00:49:52 -0400

    updating test file

--- src/test.c
+++ src/test.c
@@ -6,14 +6,23 @@
 #include <SDL3/SDL.h>
 
 int main(){
+    // pointers for the window we'll use and renderer.
     SDL_Window* window;
     SDL_Renderer* render;
-
+    
+    // this is to initialize the library & handling it if it fails.
+    // Also I want to use it's video subsystem, not anything else.
     if (!SDL_Init(SDL_INIT_VIDEO)){
         printf("library couldn't be initialized: %s\n", SDL_GetError());
         return 1;
     }
-
+    
+    // library provides a simple function to create a window and set the render to target this window. This'll connect the two.
+    // if it fails, then we say so and return.
+    if (!SDL_CreateWindowAndRenderer("My First Window", 360, 360, SDL_WINDOW_RESIZABLE, &window, &render)) {
+            printf("library couldn't be initialized: %s\n", SDL_GetError());
+            return 1;
+    }
 
     
 }
